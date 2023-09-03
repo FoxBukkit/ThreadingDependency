a99a4a28ac409d2bdd65c682edfb905a2f1c7aab
Doridian <git@doridian.net>
2023-09-02 23:27:37 -0700

    fix license

--- src/main/java/net/doridian/foxbukkit/dependencies/threading/IThreadCreator.java
+++ src/main/java/net/doridian/foxbukkit/dependencies/threading/IThreadCreator.java
@@ -1,20 +1,21 @@
-/**
- * This file is part of ThreadingDependency.
+/*
+ * threading-dependency - ${project.description}
+ * Copyright © ${year} Doridian (git@doridian.net)
  *
- * ThreadingDependency is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * ThreadingDependency is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with ThreadingDependency.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.foxelbox.dependencies.threading;
+package net.doridian.foxbukkit.dependencies.threading;
 
 public interface IThreadCreator {
 	public Thread createThread(Runnable r);
